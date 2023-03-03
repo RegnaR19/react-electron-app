f4f0bcd001e5a11f9713a8fb5d30f5d8de1aa0dc
RegnaR <nar200@yandex.ru>
2023-03-04 00:24:11 +0500

    up

--- src/components/Video/VideoPage.tsx
+++ src/components/Video/VideoPage.tsx
@@ -2,19 +2,19 @@
 
 const VideoPage = () => {
    return (
-      <>
+      <div>
          <iframe
             src="https://www.youtube.com/embed/I1hLZ2OchZ8"
             style={{ border: 0, height: '380px' }}
             width={'100%'}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allow="accelerometer; autoplay; clipboard-write;"
             allowFullScreen
          />
          <iframe
             width={'100%'} style={{ border: 0, height: '380px' }}
             src="https://www.youtube.com/embed/lBPqob1QYL4"
             title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
+            allow="accelerometer; autoplay; clipboard-write; 
          picture-in-picture; web-share" allowFullScreen></iframe>
          <iframe
             width={'100%'} style={{ border: 0, height: '380px' }}
@@ -22,7 +22,9 @@ const VideoPage = () => {
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
          picture-in-picture; web-share" allowFullScreen></iframe>
-      </>
+         <iframe width="560" height="315" src="https://www.youtube.com/embed/1LnQN0b8Feo" 
+         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
    );
 }
 
