b9ee34b7f027db14ca240e16300b811044fe183f
RegnaR <nar200@yandex.ru>
2023-03-11 11:08:50 +0500

    up

--- src/components/Profile/MainProfileContainer.tsx
+++ src/components/Profile/MainProfileContainer.tsx
@@ -1,10 +1,14 @@
+import axios from 'axios';
 import React from 'react';
 import MainProfile from './MainProfile';
 
 class MainProfileContainer extends React.Component {
 
    componentDidMount() {
-      
+      axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+         .then((response: any) => {
+            this.props.setUserProfile(response.data)
+         })
    }
 
    render() {
