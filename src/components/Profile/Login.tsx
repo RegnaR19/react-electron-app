f2f00c4e7c5279ee400054aa809c0c306a7125c3
RegnaR <nar200@yandex.ru>
2023-04-08 13:37:51 +0500

    up

--- src/components/Profile/Login.tsx
+++ src/components/Profile/Login.tsx
@@ -1,9 +1,35 @@
+import { Button, Checkbox, Input, PasswordInput } from "@mantine/core";
+import { IconAt, IconCircleKey } from "@tabler/icons-react";
+import Indent10 from "../Forms/Indent";
+import { Field, reduxForm } from "redux-form";
+
+const LoginForm = () => {
+   return (
+      <>
+         <form>
+            <Field component={"input"} placeholder="Email" />
+            <Indent10 />
+            <PasswordInput icon={<IconCircleKey />} placeholder="Пароль" autoComplete="off" />
+            <Indent10 />
+            <Checkbox label="Запомнить меня" />
+            <Indent10 />
+            <Button variant="outline">Войти</Button>
+         </form>
+      </>
+   )
+}
+
+const ReduxLoginForm = reduxForm({
+   form: 'login'
+})(LoginForm)
+
 const Login = () => {
-    return (
-        <>
-            <h2>Страница входа</h2>
-        </>
-    );
+   return (
+      <>
+         <h2>Страница входа</h2>
+         <ReduxLoginForm />
+      </>
+   );
 }
 
 export default Login;
--- vite.config.ts
+++ vite.config.ts
@@ -9,48 +9,48 @@ import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  rmSync('dist-electron', { recursive: true, force: true })
+   rmSync('dist-electron', { recursive: true, force: true })
 
-  const sourcemap = command === 'serve' || !!process.env.VSCODE_DEBUG
+   const sourcemap = command === 'serve' || !!process.env.VSCODE_DEBUG
 
-  return {
-    resolve: {
-      alias: {
-        '@': path.join(__dirname, 'src')
+   return {
+      resolve: {
+         alias: {
+            '@': path.join(__dirname, 'src')
+         },
       },
-    },
-    plugins: [
-      react(),
-      electron({
-        include: [
-          'electron'
-        ],
-        transformOptions: {
-          sourcemap,
-        },
-        plugins: [
-          ...(!!process.env.VSCODE_DEBUG
-            ? [
-              // Will start Electron via VSCode Debug
-              customStart(() => console.log(/* For `.vscode/.debug.script.mjs` */'[startup] Electron App')),
-            ]
-            : []),
-          // Allow use `import.meta.env.VITE_SOME_KEY` in Electron-Main
-          loadViteEnv(),
-        ],
-      }),
-      // Use Node.js API in the Renderer-process
-      renderer({
-        nodeIntegration: true,
-      }),
-    ],
-    server: !!process.env.VSCODE_DEBUG ? (() => {
-      const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
-      return {
-        host: url.hostname,
-        port: +url.port,
-      }
-    })() : undefined,
-    clearScreen: false,
-  }
+      plugins: [
+         react(),
+         electron({
+            include: [
+               'electron'
+            ],
+            transformOptions: {
+               sourcemap,
+            },
+            plugins: [
+               ...(!!process.env.VSCODE_DEBUG
+                  ? [
+                     // Will start Electron via VSCode Debug
+                     customStart(() => console.log(/* For `.vscode/.debug.script.mjs` */'[startup] Electron App')),
+                  ]
+                  : []),
+               // Allow use `import.meta.env.VITE_SOME_KEY` in Electron-Main
+               loadViteEnv(),
+            ],
+         }),
+         // Use Node.js API in the Renderer-process
+         renderer({
+            nodeIntegration: true,
+         }),
+      ],
+      server: !!process.env.VSCODE_DEBUG ? (() => {
+         const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
+         return {
+            host: url.hostname,
+            port: +url.port,
+         }
+      })() : undefined,
+      clearScreen: false,
+   }
 })
